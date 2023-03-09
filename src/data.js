9435090aad1e1e480789858742a43eab341f5548
gaurisharma1122 <gaurisharma1122@gmail.com>
2023-03-09 22:56:30 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,25 @@
+import img1 from "./images/img1.jpg";
+import img2 from "./images/img2.jpg";
+import img3 from "./images/img3.jpg";
+import img4 from "./images/img4.jpg";
+import img5 from "./images/img5.jpg";
+import img6 from "./images/img6.jpg";
+
+export const properties=[
+    { id: 1, title: '3140 Cherry Street Mobile, AL', image: img2, option: 'rent' }, 
+    { id: 2, title: '3140 Cherry Street Mobile, AL', image: img1, option: 'buy' }, 
+    { id: 3, title: '3140 Cherry Street Mobile, AL', image: img3, option: 'buy' }, 
+    { id: 4, title: '3140 Cherry Street Mobile, AL', image: img4, option: 'rent' }, 
+    { id: 5, title: '3140 Cherry Street Mobile, AL', image: img5, option: 'rent' }, 
+    { id: 6, title: '3140 Cherry Street Mobile, AL', image: img6, option: 'buy' } 
+];
+export const features=[
+    { id: 1, title: 'Wide Range of Properties', icon: 'fa-solid fa-sign-hanging' },
+    { id: 2, title: '15+ Agents Available', icon: 'fa-solid fa-address-card' },
+    { id: 3, title: 'Best Deals Guaranteed', icon: 'fa-solid fa-tags' },
+];
+export const steps=[
+    { id: 1, title: 'Drop us mail', text: 'Lorem ipsum dolor sit ametconsetetur sadipscing nonumy eirmod', icon: 'fa-solid fa-envelope' },
+    { id: 2, title: 'Select your location', text: 'Lorem ipsum dolor sit ametconsetetur sadipscing nonumy eirmod', icon: 'fa-solid fa-location-dot' },
+    { id: 3, title: 'Rent or Buy', text: 'Lorem ipsum dolor sit ametconsetetur sadipscing nonumy eirmod', icon: 'fa-solid fa-house' },
+];
